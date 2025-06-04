4933cc00c56d6f01b3f1c924cbeaf3fa7007d8c6
clasilv <165424582+clasilv@users.noreply.github.com>
2025-06-03 21:00:08 -0300

    Update agendamentoDAO.java

--- src/dao/agendamentoDAO.java
+++ src/dao/agendamentoDAO.java
@@ -1,1 +1,10 @@
+package com.exemplo.agendamedica.repository;
 
+import com.exemplo.agendamedica.model.AgendamentoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AgendamentoRepository extends JpaRepository<AgendamentoModel, Long> {
+
+}
